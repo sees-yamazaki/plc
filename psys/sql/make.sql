e0acff25855d57454d5749e0dfb8f40a8f40dce3
sees-yamazaki <yamazaki@sees.yokohama>
2020-01-29 08:58:36 +0900

    本環境構築SQL

--- psys/sql/make.sql
+++ psys/sql/make.sql
@@ -19,6 +19,7 @@ SET time_zone = "+00:00";
 --
 -- テーブルの構造 `games`
 --
+DROP TABLE IF EXISTS `games`;
 
 CREATE TABLE `games` (
   `g_seq` int(11) NOT NULL,
@@ -40,6 +41,7 @@ CREATE TABLE `games` (
 --
 -- テーブルの構造 `hitcounts`
 --
+DROP TABLE IF EXISTS `hitcounts`;
 
 CREATE TABLE `hitcounts` (
   `hc_seq` int(11) NOT NULL,
@@ -58,6 +60,7 @@ CREATE TABLE `hitcounts` (
 --
 -- テーブルの構造 `infos`
 --
+DROP TABLE IF EXISTS `infos`;
 
 CREATE TABLE `infos` (
   `inf_seq` int(11) NOT NULL,
@@ -81,6 +84,7 @@ CREATE TABLE `infos` (
 --
 -- テーブルの構造 `log_login`
 --
+DROP TABLE IF EXISTS `log_login`;
 
 CREATE TABLE `log_login` (
   `logindt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -97,6 +101,7 @@ CREATE TABLE `log_login` (
 --
 -- テーブルの構造 `members`
 --
+DROP TABLE IF EXISTS `members`;
 
 CREATE TABLE `members` (
   `m_seq` int(11) NOT NULL,
@@ -121,6 +126,7 @@ CREATE TABLE `members` (
 --
 -- テーブルの構造 `prizes`
 --
+DROP TABLE IF EXISTS `prizes`;
 
 CREATE TABLE `prizes` (
   `pz_seq` int(11) NOT NULL,
@@ -145,6 +151,7 @@ CREATE TABLE `prizes` (
 --
 -- テーブルの構造 `promos`
 --
+DROP TABLE IF EXISTS `promos`;
 
 CREATE TABLE `promos` (
   `p_seq` int(11) NOT NULL,
@@ -168,6 +175,7 @@ CREATE TABLE `promos` (
 --
 -- テーブルの構造 `serialcodes`
 --
+DROP TABLE IF EXISTS `serialcodes`;
 
 CREATE TABLE `serialcodes` (
   `sc_seq` int(11) NOT NULL,
@@ -188,6 +196,7 @@ CREATE TABLE `serialcodes` (
 --
 -- テーブルの構造 `serials`
 --
+DROP TABLE IF EXISTS `serials`;
 
 CREATE TABLE `serials` (
   `s_seq` int(11) NOT NULL,
@@ -207,6 +216,7 @@ CREATE TABLE `serials` (
 --
 -- テーブルの構造 `ships`
 --
+DROP TABLE IF EXISTS `ships`;
 
 CREATE TABLE `ships` (
   `sp_seq` int(11) NOT NULL,
@@ -235,6 +245,7 @@ CREATE TABLE `ships` (
 --
 -- テーブルの構造 `systems`
 --
+DROP TABLE IF EXISTS `systems`;
 
 CREATE TABLE `systems` (
   `path_root` varchar(100) NOT NULL,
@@ -261,6 +272,7 @@ INSERT INTO `systems` (`path_root`, `path_promo`, `path_game`, `path_info`, `pat
 --
 -- テーブルの構造 `usepoints`
 --
+DROP TABLE IF EXISTS `usepoints`;
 
 CREATE TABLE `usepoints` (
   `up_seq` int(11) NOT NULL,
@@ -282,6 +294,7 @@ CREATE TABLE `usepoints` (
 --
 -- テーブルの構造 `users`
 --
+DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
   `users_seq` int(11) NOT NULL,
@@ -604,6 +617,7 @@ ALTER TABLE `users`
 -- ビュー用の構造 `v_lastlogin`
 --
 DROP TABLE IF EXISTS `v_lastlogin`;
+DROP VIEW IF EXISTS `v_lastlogin`;
 
 CREATE  VIEW `v_lastlogin`  AS  select max(`log_login`.`logindt`) AS `logindt`,`log_login`.`m_seq` AS `m_seq` from `log_login` group by `log_login`.`m_seq` ;
 
@@ -613,6 +627,7 @@ CREATE  VIEW `v_lastlogin`  AS  select max(`log_login`.`logindt`) AS `logindt`,`
 -- ビュー用の構造 `v_members`
 --
 DROP TABLE IF EXISTS `v_members`;
+DROP VIEW IF EXISTS `v_members`;
 
 CREATE  VIEW `v_members`  AS  select `m`.`m_seq` AS `m_seq`,`m`.`m_id` AS `m_id`,`m`.`m_pw` AS `m_pw`,`m`.`m_name` AS `m_name`,`m`.`m_mail` AS `m_mail`,`m`.`m_post` AS `m_post`,`m`.`m_address1` AS `m_address1`,`m`.`m_address2` AS `m_address2`,`m`.`m_tel` AS `m_tel`,`m`.`createdt` AS `createdt`,(case isnull((((`sc`.`sc_point` - `up99`.`up_point_99`) - `up0`.`up_point_0`) - `up1`.`up_point_1`)) when 1 then 0 else (((`sc`.`sc_point` - `up99`.`up_point_99`) - `up0`.`up_point_0`) - `up1`.`up_point_1`) end) AS `crnt_point`,(case isnull(`sc`.`sc_cnt`) when 1 then 0 else `sc`.`sc_cnt` end) AS `sc_cnt`,(case isnull(`sc`.`sc_point`) when 1 then 0 else `sc`.`sc_point` end) AS `sc_point`,(case isnull(`up0`.`cnt_0`) when 1 then 0 else `up0`.`cnt_0` end) AS `cnt_0`,(case isnull(`up0`.`up_point_0`) when 1 then 0 else `up0`.`up_point_0` end) AS `up_point_0`,(case isnull(`up1`.`cnt_1`) when 1 then 0 else `up1`.`cnt_1` end) AS `cnt_1`,(case isnull(`up1`.`up_point_1`) when 1 then 0 else `up1`.`up_point_1` end) AS `up_point_1`,(case isnull(`up99`.`cnt_99`) when 1 then 0 else `up99`.`cnt_99` end) AS `cnt_99`,(case isnull(`up99`.`up_point_99`) when 1 then 0 else `up99`.`up_point_99` end) AS `up_point_99`,`l`.`logindt` AS `logindt` from (((((`members` `m` left join (select `serialcodes`.`m_seq` AS `m_seq`,count(0) AS `sc_cnt`,sum(`serialcodes`.`sc_point`) AS `sc_point` from `serialcodes` where (`serialcodes`.`m_seq` is not null) group by `serialcodes`.`m_seq`) `sc` on((`sc`.`m_seq` = `m`.`m_seq`))) left join (select `usepoints`.`m_seq` AS `m_seq`,count(0) AS `cnt_0`,sum(`usepoints`.`up_point`) AS `up_point_0` from `usepoints` where (`usepoints`.`up_status` = 0) group by `usepoints`.`m_seq`) `up0` on((`up0`.`m_seq` = `m`.`m_seq`))) left join (select `usepoints`.`m_seq` AS `m_seq`,count(0) AS `cnt_1`,sum(`usepoints`.`up_point`) AS `up_point_1` from `usepoints` where (`usepoints`.`up_status` = 1) group by `usepoints`.`m_seq`) `up1` on((`up1`.`m_seq` = `m`.`m_seq`))) left join (select `usepoints`.`m_seq` AS `m_seq`,count(0) AS `cnt_99`,sum(`usepoints`.`up_point`) AS `up_point_99` from `usepoints` where (`usepoints`.`up_status` = 99) group by `usepoints`.`m_seq`) `up99` on((`up99`.`m_seq` = `m`.`m_seq`))) left join `v_lastlogin` `l` on((`l`.`m_seq` = `m`.`m_seq`))) ;
 
@@ -622,6 +637,7 @@ CREATE  VIEW `v_members`  AS  select `m`.`m_seq` AS `m_seq`,`m`.`m_id` AS `m_id`
 -- ビュー用の構造 `v_point`
 --
 DROP TABLE IF EXISTS `v_point`;
+DROP VIEW IF EXISTS `v_point`;
 
 CREATE  VIEW `v_point`  AS  select `a`.`m_seq` AS `m_seq`,sum(`a`.`point`) AS `point` from (select `serialcodes`.`m_seq` AS `m_seq`,sum(`serialcodes`.`sc_point`) AS `point` from `serialcodes` where (`serialcodes`.`m_seq` is not null) group by `serialcodes`.`m_seq` union select `usepoints`.`m_seq` AS `m_seq`,(sum(`usepoints`.`up_point`) * -(1)) AS `point` from `usepoints` group by `usepoints`.`m_seq`) `a` group by `a`.`m_seq` ;
 
@@ -631,6 +647,7 @@ CREATE  VIEW `v_point`  AS  select `a`.`m_seq` AS `m_seq`,sum(`a`.`point`) AS `p
 -- ビュー用の構造 `v_prizes`
 --
 DROP TABLE IF EXISTS `v_prizes`;
+DROP VIEW IF EXISTS `v_prizes`;
 
 CREATE  VIEW `v_prizes`  AS  select `p`.`pz_seq` AS `pz_seq`,`p`.`p_seq` AS `p_seq`,`p`.`pz_order` AS `pz_order`,`p`.`pz_title` AS `pz_title`,`p`.`pz_code` AS `pz_code`,`p`.`pz_img` AS `pz_img`,`p`.`pz_text` AS `pz_text`,`p`.`pz_nowcnt` AS `pz_nowcnt`,`p`.`pz_kind` AS `pz_kind`,`h`.`hc_no` AS `hc_no` from (`prizes` `p` left join (select `hitcounts`.`p_seq` AS `p_seq`,`hitcounts`.`pz_seq` AS `pz_seq`,group_concat(`hitcounts`.`hc_no` separator ',') AS `hc_no` from `hitcounts` group by `hitcounts`.`p_seq`,`hitcounts`.`pz_seq`) `h` on(((`p`.`p_seq` = `h`.`p_seq`) and (`p`.`pz_seq` = `h`.`pz_seq`)))) ;
 
@@ -640,6 +657,7 @@ CREATE  VIEW `v_prizes`  AS  select `p`.`pz_seq` AS `pz_seq`,`p`.`p_seq` AS `p_s
 -- ビュー用の構造 `v_serialcodes`
 --
 DROP TABLE IF EXISTS `v_serialcodes`;
+DROP VIEW IF EXISTS `v_serialcodes`;
 
 CREATE  VIEW `v_serialcodes`  AS  select `s`.`s_seq` AS `s_seq`,`s`.`s_title` AS `s_title`,`s`.`s_qty` AS `s_qty`,`s`.`createdt` AS `createdt`,`s`.`users_seq` AS `users_seq`,`sc`.`sc_seq` AS `sc_seq`,`sc`.`sc_code` AS `sc_code`,`sc`.`entrydt` AS `entrydt`,`sc`.`sc_point` AS `sc_point`,`sc`.`m_seq` AS `m_seq`,`m`.`m_name` AS `m_name` from ((`serials` `s` left join `serialcodes` `sc` on((`s`.`s_seq` = `sc`.`s_seq`))) left join `members` `m` on((`sc`.`m_seq` = `m`.`m_seq`))) ;
 
@@ -649,6 +667,7 @@ CREATE  VIEW `v_serialcodes`  AS  select `s`.`s_seq` AS `s_seq`,`s`.`s_title` AS
 -- ビュー用の構造 `v_ships`
 --
 DROP TABLE IF EXISTS `v_ships`;
+DROP VIEW IF EXISTS `v_ships`;
 
 CREATE  VIEW `v_ships`  AS  select `ships`.`sp_seq` AS `sp_seq`,`ships`.`m_seq` AS `m_seq`,`ships`.`up_seq` AS `up_seq`,`ships`.`sp_name` AS `sp_name`,`ships`.`sp_post` AS `sp_post`,`ships`.`sp_address1` AS `sp_address1`,`ships`.`sp_address2` AS `sp_address2`,`ships`.`sp_tel` AS `sp_tel`,`ships`.`sp_text` AS `sp_text`,`ships`.`sp_flg` AS `sp_flg`,`ships`.`createdt` AS `createdt`,`v_usepoints`.`p_title` AS `p_title`,`v_usepoints`.`pz_title` AS `pz_title`,`v_usepoints`.`pz_code` AS `pz_code`,`members`.`m_name` AS `m_name`,`members`.`m_mail` AS `m_mail`,`members`.`m_post` AS `m_post`,`members`.`m_address1` AS `m_address1`,`members`.`m_address2` AS `m_address2`,`members`.`m_tel` AS `m_tel` from ((`ships` left join `v_usepoints` on((`ships`.`up_seq` = `v_usepoints`.`up_seq`))) left join `members` on((`members`.`m_seq` = `ships`.`m_seq`))) ;
 
@@ -658,6 +677,7 @@ CREATE  VIEW `v_ships`  AS  select `ships`.`sp_seq` AS `sp_seq`,`ships`.`m_seq` 
 -- ビュー用の構造 `v_unships`
 --
 DROP TABLE IF EXISTS `v_unships`;
+DROP VIEW IF EXISTS `v_unships`;
 
 CREATE  VIEW `v_unships`  AS  select `usepoints`.`up_seq` AS `up_seq`,`usepoints`.`m_seq` AS `m_seq`,`usepoints`.`up_point` AS `up_point`,`usepoints`.`createdt` AS `createdt`,`usepoints`.`up_status` AS `up_status`,`usepoints`.`g_seq` AS `g_seq`,`usepoints`.`p_seq` AS `p_seq`,`usepoints`.`pz_seq` AS `pz_seq` from (`usepoints` left join `ships` on((`usepoints`.`up_seq` = `ships`.`up_seq`))) where ((`usepoints`.`up_status` = 1) and isnull(`ships`.`sp_seq`)) ;
 
@@ -667,5 +687,6 @@ CREATE  VIEW `v_unships`  AS  select `usepoints`.`up_seq` AS `up_seq`,`usepoints
 -- ビュー用の構造 `v_usepoints`
 --
 DROP TABLE IF EXISTS `v_usepoints`;
+DROP VIEW IF EXISTS `v_usepoints`;
 
 CREATE  VIEW `v_usepoints`  AS  select `u`.`up_seq` AS `up_seq`,`u`.`m_seq` AS `m_seq`,`u`.`up_point` AS `up_point`,`u`.`createdt` AS `createdt`,`u`.`up_status` AS `up_status`,`u`.`g_seq` AS `g_seq`,`u`.`p_seq` AS `p_seq`,`u`.`pz_seq` AS `pz_seq`,`m`.`m_name` AS `m_name`,`p`.`p_title` AS `p_title`,`pz`.`pz_title` AS `pz_title`,`pz`.`pz_code` AS `pz_code` from (((`usepoints` `u` left join `members` `m` on((`u`.`m_seq` = `m`.`m_seq`))) left join `promos` `p` on((`p`.`p_seq` = `u`.`p_seq`))) left join `prizes` `pz` on((`u`.`pz_seq` = `pz`.`pz_seq`))) ;
