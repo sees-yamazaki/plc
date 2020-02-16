0f95d502725315e037c83621b8b954b86510b450
sees5335 <yamazaki@sees.yokohama>
2020-02-16 15:37:53 +0900

    20200216

--- aflasp/x10c_header_cuser.php
+++ aflasp/x10c_header_cuser.php
@@ -75,55 +75,56 @@
     -->
 
 
-    <div class="page-body">
-        <div class="sidebar">
-            <ul class="navigation-menu">
-                <li>
-                    <a href="index.php" class="menu">
-                        <span class="link-title">ホーム</span>
-                        <i class="mdi mdi-home link-icon"></i>
-                    </a>
-                </li>
-                <li>
-                    <a href="#pages2" data-toggle="collapse" aria-expanded="false" class="menu">
-                        <span class="link-title">広告の掲載</span>
-                        <i class="mdi mdi-account-multiple link-icon"></i>
-                    </a>
-                    <ul class="collapse navigation-submenu" id="pages2">
-                        <li><a href="x10c_adwares_edit.php" class="menu">新しい広告を登録</a></li>
-                        <li><a href="x10c_offerad_search.php" class="menu">過去に登録した広告</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#pages3" data-toggle="collapse" aria-expanded="false" class="menu">
-                        <span class="link-title">累計</span>
-                        <i class="mdi mdi-account-multiple link-icon"></i>
-                    </a>
-                    <ul class="collapse navigation-submenu" id="pages3">
-                        <li><a href="search.php?type=access&run=true" class="menu">広告へのアクセス履歴</a></li>
-                        <li><a href="search.php?type=pay&run=true" class="menu">獲得報酬の発生履歴</a></li>
-                        <li><a href="search.php?type=click_pay&run=true" class="menu">クリック報酬の発生履歴</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#pages4" data-toggle="collapse" aria-expanded="false" class="menu">
-                        <span class="link-title">ユーザー情報</span>
-                        <i class="mdi mdi-account-multiple link-icon"></i>
-                    </a>
-                    <ul class="collapse navigation-submenu" id="pages4">
-                        <li><a href="edit.php?type=cUser&id=(!--# val LOGIN_ID #--)" class="menu">ユーザー情報の編集</a></li>
-                        <li><a href="other.php?key=pay_report" class="menu">成果レポート出力</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="login.php?logout=true" class="menu">
-                        <span class="link-title">ログオフ</span>
-                        <i class="mdi mdi-home link-icon"></i>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
+                <div class="page-body">
+                    <div class="sidebar">
+                        <ul class="navigation-menu">
+                            <li>
+                                <a href="index.php" class="menu">
+                                    <span class="link-title">ホーム</span>
+                                    <i class="mdi mdi-home link-icon"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#pages2" data-toggle="collapse" aria-expanded="false" class="menu">
+                                    <span class="link-title">広告の掲載</span>
+                                    <i class="mdi mdi-content-copy link-icon"></i>
+                                </a>
+                                <ul class="collapse navigation-submenu" id="pages2">
+                                    <li><a href="x10c_adwares_edit.php" class="menu">新しい広告を登録</a></li>
+                                    <li><a href="x10c_offerad_search.php" class="menu">過去に登録した広告</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#pages3" data-toggle="collapse" aria-expanded="false" class="menu">
+                                    <span class="link-title">累計</span>
+                                    <i class="mdi mdi-file-document link-icon"></i>
+                                </a>
+                                <ul class="collapse navigation-submenu" id="pages3">
+                                    <li><a href="search.php?type=access&run=true" class="menu">広告へのアクセス履歴</a></li>
+                                    <li><a href="search.php?type=pay&run=true" class="menu">獲得報酬の発生履歴</a></li>
+                                    <li><a href="search.php?type=click_pay&run=true" class="menu">クリック報酬の発生履歴</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#pages4" data-toggle="collapse" aria-expanded="false" class="menu">
+                                    <span class="link-title">ユーザー情報</span>
+                                    <i class="mdi mdi-account-circle link-icon"></i>
+                                </a>
+                                <ul class="collapse navigation-submenu" id="pages4">
+                                    <li><a href="edit.php?type=cUser&id=(!--# val LOGIN_ID #--)"
+                                            class="menu">ユーザー情報の編集</a></li>
+                                    <li><a href="other.php?key=pay_report" class="menu">成果レポート出力</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="login.php?logout=true" class="menu">
+                                    <span class="link-title">ログオフ</span>
+                                    <i class="mdi mdi-logout link-icon"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
 
 
 
